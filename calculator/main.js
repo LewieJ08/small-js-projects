d8e7a36ddf6583a743ed4c803cef5a6032256cf9
LewieJ08 <lewiej08@gmail.com>
2025-05-23 22:22:01 +0100

    feat: added calc project

---
+++ calculator/main.js
@@ -0,0 +1,67 @@
+const prompt = require("prompt-sync")();
+
+function add(x, y) {
+    return (x + y);
+}
+
+function subtract(x, y) {
+    return (x - y);
+}
+
+function multiply(x, y) {
+    return (x * y);
+}
+
+function divide(x, y) {
+    if (y === 0) {
+        return undefined;
+    }
+
+    return (x / y);
+}
+
+function main() {       
+    while (true) {
+
+        console.log("\n----Calulator----");
+        console.log("1. Addition");
+        console.log("2. Subtraction");
+        console.log("3. Multiplication");
+        console.log("4. Division");
+        console.log("5. Exit");
+
+        let operator = Number(prompt("Enter an operator (1-5) > "));
+
+        if (operator === NaN) {
+            console.log("Input must be a number, please try again");
+            continue;
+        }
+        else if (operator === 5) {
+            console.log("Exiting...");
+            break;
+        }
+
+        let num1 = Number(prompt("Enter the first number > "));
+        let num2 = Number(prompt("Enter the second number> "));
+        
+        switch(operator) {
+            case 1:
+                console.log(`${num1} + ${num2} = ${add(num1, num2)}`);
+                break;
+            case 2:
+                console.log(`${num1} - ${num2} = ${subtract(num1, num2)}`);
+                break;
+            case 3:
+                console.log(`${num1} * ${num2} = ${multiply(num1, num2)}`);
+                break;
+            case 4:
+                console.log(`${num1} / ${num2} = ${divide(num1, num2)}`);
+                break;
+            default:
+                console.log("Invalid input, please try again")
+                continue;
+        }
+    }
+}
+
+main()
