fd8696cc4fb89a02d7065471b1e73c1bac64294d
LewieJ08 <lewiej08@gmail.com>
2025-05-31 03:50:51 +0100

    feat: finished compound interest calc

--- compound-interest/main.js
+++ compound-interest/main.js
@@ -1,7 +1,28 @@
 const prompt = require("prompt-sync")();
 
+function calc(baseValue, interestRate, timeFrame) {
+    const interestMultipler = interestRate + 1;
+    let subTotal = Math.pow(interestMultipler, timeFrame)
+
+    return baseValue * subTotal
+}
+
 function main() {
-    let baseValue = prompt("")
+    while (true) {
+        const baseValue = Number(prompt("Enter Base Value > "));
+        const interestRate = Number(prompt("Enter Interest Rate (Decimal) > "));
+        const timeFrame = Number(prompt("Enter Time Frame (Years) > "));
+
+        if (isNaN(baseValue) || isNaN(interestRate) || isNaN(timeFrame)) {
+            console.log("Error, One or more inputs are Invalid and MUST be an Integer.");
+            continue;
+        }
+        else {
+            total = calc(baseValue, interestRate, timeFrame)
+            console.log(`Your total amount after ${timeFrame} year(s) will be: ${total}`);
+            break;
+        } 
+    }
 }
 
 main()
