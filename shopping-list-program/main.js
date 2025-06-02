783cddef5296fd75fdd888bcfd230e2fbe7a17b3
LewieJ08 <lewiej08@gmail.com>
2025-06-02 10:10:52 +0100

    update: main.js

--- shopping-list-program/main.js
+++ shopping-list-program/main.js
@@ -7,13 +7,35 @@ function main() {
     let total = 0;
 
     while (true) {
-        const food = prompt("Enter a food (q to quit) > ");
+        const food = prompt("Enter a food (q to quit) > ").toLowerCase();
 
         if (food === "q") {
-            console.log("Exiting...");
+            console.log("Getting final summary...");
             break;
         }
+
+        const price = Number(prompt(`Enter the price of ${food} > `));
+
+        if (isNaN(price)) {
+            console.log("Invalid input, price must be an integer.");
+            continue;
+        }
+
+        foods.push(food)
+        prices.push(price)        
     }
+    
+    for (let price in prices) {
+        total += prices[price];
+    }
+    
+    console.log("\nFoods:")
+
+    for (let i = 1; i == foods.length; i++) {
+        console.log(`Food ${i}: ${foods[i+1]} £${prices[i+1]}`);
+    }
+
+    console.log(`Total price: £${total}`);
 } 
 
 main()
