d37290dc4c809eb6512869779294fd840cfe37c7
LewieJ08 <lewiej08@gmail.com>
2025-06-02 16:07:21 +0100

    update: main.js

--- quiz-game/main.js
+++ quiz-game/main.js
@@ -1,5 +1,17 @@
 const prompt = require("prompt-sync")();
 
+const questions = ["What is the capital city of France?",
+                   "Which planet is known as the Red Planet?",
+                   "What do bees make?",
+                   "How many legs does a spider have?",
+                   "Which ocean is the largest?"]
+
+const options = [["A) Madrid","B) Paris","C) Rome"],
+                 ["A) Earth","B) Venus","C) Mars"],
+                 ["A) Honey","B) Milk","C) Butter"],
+                 ["","",""],
+                 ["","",""]]
+
 function main() {
     // pass
 }
