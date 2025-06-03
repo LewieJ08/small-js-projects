738e95a773d8c96653833a6433191472c4fde1e1
LewieJ08 <lewiej08@gmail.com>
2025-06-03 13:11:59 +0100

    feat: finish quiz game

--- quiz-game/main.js
+++ quiz-game/main.js
@@ -4,16 +4,45 @@ const questions = ["What is the capital city of France?",
                    "Which planet is known as the Red Planet?",
                    "What do bees make?",
                    "How many legs does a spider have?",
-                   "Which ocean is the largest?"]
+                   "Which ocean is the largest?"];
 
 const options = [["A) Madrid","B) Paris","C) Rome"],
                  ["A) Earth","B) Venus","C) Mars"],
                  ["A) Honey","B) Milk","C) Butter"],
-                 ["","",""],
-                 ["","",""]]
+                 ["A) 6","B) 8","C) 10"],
+                 ["A) Atlantic Ocean","B) Indian Ocean","C) Pacific Ocean"]];
+
+const answers = ["B","C","A","B","C"];
 
 function main() {
-    // pass
+    let score = 0
+
+    let i = 0
+    for (let question of questions) {
+        console.log(question);
+        console.log(options[i]);
+
+        while (true) {
+            userAnswer = prompt("> ").toUpperCase();
+
+            if (userAnswer === answers[i]) {
+                console.log("Correct!");
+                score += 1;
+                break;
+            }
+            else if (!(answers.includes(userAnswer))) {
+                console.log("Invalid input, must be A, B or C.");
+                continue;
+            }
+            else {
+                console.log("Incorrect.");
+                break;
+            }
+        }
+
+        i += 1
+    }
+    
 }
 
 main()
