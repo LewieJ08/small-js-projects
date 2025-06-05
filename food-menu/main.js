a5b6de70b03ab72e21bc276d79311368f086213e
LewieJ08 <lewiej08@gmail.com>
2025-06-05 15:32:44 +0100

    feat: pre receipt func

--- food-menu/main.js
+++ food-menu/main.js
@@ -1,29 +1,44 @@
 const prompt = require("prompt-sync")();
 
 const menu = {
-    "Hamburger": 2.00,
-    "Cheeseburger": 3.00,
-    "Chicken Wrap": 3.50,
-    "Chips": 1.50,
-    "Soft Drink": 2.20,
-    "Hot Drink": 2.50,
+    "1": ["Hamburger", 2.00],
+    "2": ["Cheeseburger", 2.50],
+    "3": ["Chicken Wrap", 3.30],
+    "4": ["Chips", 1.50],
+    "5": ["Pasta", 1.80],
+    "6": ["Soft Drink", 2.30],
+    "7": ["Hot Drink", 3.00],
 };
+
+function printReceipt(foods, totalPrice) {
+    // pass
+}
 
 function main() {
 
     let foods = [];
+    let prices = [];
 
-    for (const [menuFood, price] of Object.entries(menu)) {
-        console.log(`${menuFood} £${price}`);
+    for (const [id,[menuFood, price]] of Object.entries(menu)) {
+        console.log(`${id}. ${menuFood} £${price}`);
     }
 
     while (true) {
-        const food = prompt("Enter a food (q to quit) > ").toLowerCase();
+        const id = prompt("Enter a food ID (q to quit) > ").toLowerCase();
 
-        if (!Object.keys(menu).includes(food)) {
+        if (id === "q") {
+            return printReceipt(foods,prices);
+        }
+
+        else if (!Object.keys(menu).includes(id)) {
             console.log("Invalid input. Food not on menu.");
             continue;
         }
+
+        foods.push(menu[id][0]);
+        prices.push(menu[id][1]);
+
+        console.log(`${menu[id][0]} added.`);
     }
 }
 
