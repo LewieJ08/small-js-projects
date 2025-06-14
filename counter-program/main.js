02d9fff0dce1c707256fe19d622f4e7e3da43355
LewieJ08 <lewiej08@gmail.com>
2025-06-14 03:32:51 +0100

    feat: finish program

--- counter-program/main.js
+++ counter-program/main.js
@@ -1,3 +1,24 @@
 const addButton = document.getElementById("addButton");
 const subractButton = document.getElementById("subtractButton");
 const resetButton = document.getElementById("resetButton");
+const count = document.getElementById("count");
+
+function main() {
+    let countNum = Number(document.getElementById("count").innerHTML);
+
+    addButton.addEventListener("click", function () {
+        countNum++;
+        count.innerHTML = countNum;
+    });
+
+    subractButton.addEventListener("click", function () {
+        countNum--;
+        count.innerHTML = countNum;
+    });
+
+    resetButton.addEventListener("click", function () {
+        count.innerHTML = "0";
+    });
+}
+
+main()
