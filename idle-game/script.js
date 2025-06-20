36db8f0fed208b98a2ef65a5575ce07f4757f998
LewieJ08 <lewiej08@gmail.com>
2025-06-20 14:11:42 +0100

    feat: start idle game

---
+++ idle-game/script.js
@@ -0,0 +1,32 @@
+const button = document.getElementById("button");
+const valueH1 = document.getElementById("value");
+const cursorButton = document.getElementById("cursor");
+const cpsH3 = document.getElementById("cps");
+
+let cps = 0;
+let value = 0;
+
+function updateHTML() {
+    valueH1.innerHTML = value;
+    cpsH3.innerHTML = cps; 
+}
+
+button.addEventListener("click", function () {
+    value++;
+    updateHTML()
+});
+
+// SHOP BUTTONS
+
+// Cursor Func
+cursorButton.addEventListener("click", function () {
+    if (value >= 5) {
+        value -= 5;
+        cps += 1;
+        updateHTML();
+    }
+    else {
+        window.alert("Not Enough Funds");
+    }
+});
+
