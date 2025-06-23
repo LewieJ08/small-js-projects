8c3d5b56fe32ce7abceea8ab26ce660608b21498
LewieJ08 <lewiej08@gmail.com>
2025-06-23 17:15:48 +0100

    feat: price increase formula

--- idle-game/script.js
+++ idle-game/script.js
@@ -3,26 +3,45 @@ const valueH1 = document.getElementById("value");
 const cursorButton = document.getElementById("cursor");
 const cpsH3 = document.getElementById("cps");
 
+// Building Costs
+const cursorCost = document.getElementById("cursorCost")
+
+// Building Amounts & New Costs
+
+let cursorNum = 0;
+let cursorNewCost;
+
 let cps = 0;
 let value = 0;
 
 function updateHTML() {
     valueH1.innerHTML = Math.round(value * 10) / 10;
     cpsH3.innerHTML = Math.round(cps * 10) / 10; 
+
+    if (isNaN(cursorNewCost)) {
+        cursorNewCost = 15;
+    }
+    
+    cursorCost.innerHTML = Math.round(cursorNewCost * 10) / 10;
+}
+
+function priceIncrease(baseCost, buildingNum) {
+    const price = baseCost * Math.pow(1.15, buildingNum);
+    return price;
 }
 
 // CPS Func + CPS Interval 
 
 function CpsFunc() {
-    value += cps
-    updateHTML()
+    value += cps;
+    updateHTML();
 }
 
 setInterval(CpsFunc, 1000)
 
 button.addEventListener("click", function () {
     value++;
-    updateHTML()
+    updateHTML();
 });
 
 // SHOP BUTTONS
@@ -32,6 +51,8 @@ cursorButton.addEventListener("click", function () {
     if (value >= 15) {
         value -= 15;
         cps += 0.1;
+        cursorNum += 1;
+        cursorNewCost = priceIncrease(15, cursorNum);
         updateHTML();
     }
     else {
